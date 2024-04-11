6b2952359af965da2c2fb649430fada10d32d04c
Kavindu Pramod <kavindu.20210410@iit.ac.lk>
2024-04-11 12:17:07 +0530

    Update API base URL in config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const devConfig = {
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://165.22.97.154:3001/",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://165.22.97.154:3001",
 };
 
 const prodConfig = {
