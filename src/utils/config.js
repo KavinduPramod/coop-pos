368a8ef371a1735197899aeb2418eea016bf51ea
Kavindu Pramod <kavindu.20210410@iit.ac.lk>
2024-04-17 15:46:26 +0530

    Update API base URL in config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,8 +3,8 @@ const devConfig = {
   baseURL: process.env.REACT_APP_API_BASE_URL || "https://coop-pos-7dc6d9047bd4.herokuapp.com",
 };
 
-const prodConfig = {
-  baseURL: "Your production url",
-};
+// const prodConfig = {
+//   baseURL: "Your production url",
+// };
 
 export const config = devConfig;
