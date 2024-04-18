325de91b18f5ccd370bc31d2edb139c2424e1995
Kavindu Pramod <kavindu.20210410@iit.ac.lk>
2024-04-18 15:37:38 +0530

    Refactor LastTransactionPage component in index.js

--- src/pages/LastTransaction/index.js
+++ src/pages/LastTransaction/index.js
@@ -1,29 +1,28 @@
-import { React, useCallback, useState, useEffect, useRef } from 'react';
+import { React, useState, useEffect, useRef } from 'react';
 import './style.css';
 import { Api } from '../../utils/Api';
 import Menu from '../../components/Menu';
 import Title from '../../components/Title';
-import swal from 'sweetalert';
 import html2canvas from 'html2canvas';
 
 const LastTransactionPage = () => {
-
   const [loginUserBranchID, setLogInUserBranchID] = useState();
   const [loginUserID, setLogInUserID] = useState();
   const [loginUserName, setLogInUserName] = useState();
   const [transactionData, setTransactionData] = useState([]);
-  const [transactionDataSize, setTransactionDataSize] = useState();
+  const [transactionDataSize, setTransactionDataSize] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
-  const [amount, setAmount] = useState(0);
-  const [activeTab, setActiveTab] = useState("tranaction");
+  const [activeTab, setActiveTab] = useState("transaction");
   const printElementRef = useRef(null);
 
   useEffect(() => {
-    let data = JSON.parse(sessionStorage.getItem('userKey'));
-    setLogInUserBranchID(data.branch_id);
-    setLogInUserID(data.user_id);
-    setLogInUserName(data.user_name);
-    const fetchTransactions = async () => {
+    // Fetch user data and transactions on component mount
+    const fetchData = async () => {
+      let data = JSON.parse(sessionStorage.getItem('userKey'));
+      setLogInUserBranchID(data.branch_id);
+      setLogInUserID(data.user_id);
+      setLogInUserName(data.user_name);
+      
       let date = new Date();
       let month = (date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1);
       let to_date = date.getDate() < 10 ? "0" + date.getDate() + 1 : date.getDate();
@@ -34,24 +33,30 @@ const LastTransactionPage = () => {
         user_id: data.user_id,
         date: dateFormat
       };
+      
       const response = await Api.postRequest('/api/transaction/findTransaction', body);
-      (response.code == 404) ? setTransactionDataSize(0) : setTransactionData(response);
-
+      if (response.code === 404) {
+        setTransactionDataSize(0);
+      } else {
+        setTransactionData(response);
+      }
+      
       setIsLoading(false);
     };
-    fetchTransactions();
-        // Define the 'lee' object and its method 'funAndroid' globally
-        window.lee = {
-          funAndroid: function(base64Data) {
-            // This method will be called when invoked from JavaScript
-            console.log("Received data from JavaScript:", base64Data);
-            // Additional logic for handling the received data
-          }
-        };
+
+    fetchData();
+
+    // Define the 'lee' object and its method 'funAndroid' globally
+    window.lee = {
+      funAndroid: function(base64Data) {
+        // This method will be called when invoked from JavaScript
+        console.log("Received data from JavaScript:", base64Data);
+        // Additional logic for handling the received data
+      }
+    };
   }, []);
 
   const handleClick = (value) => {
-    console.log("Button clicked with value:", value);
     setActiveTab(value);
   };
 
@@ -76,17 +81,17 @@ const LastTransactionPage = () => {
             <label className='mainTitle'>Last Transactions</label>
           </div>
           <div className='col-6 tab_left'>
-            <button className='btn btn_tab' type='button' onClick={() => handleClick("tranaction")}>Transactions</button>
+            <button className='btn btn_tab' type='button' onClick={() => handleClick("transaction")}>Transactions</button>
           </div>
           <div className='col-6 tab_right'>
             <button className='btn btn_tab' type='button' onClick={() => handleClick("summary")}>Summary</button>
           </div>
         </div>
-        <div className={activeTab === "tranaction" ? "deactive" : "tab_active"}>
+        <div className={activeTab === "transaction" ? "deactive" : "tab_active"}>
           <div className='row'>
             <div className='col-12'>
               <div className='card card_info card-info-black-and-white' ref={printElementRef}>
-                <div className='date'>Akila Kumara</div>
+                <div className='date'>{loginUserName}</div>
                 <div className='date'>2024.01.23</div>
                 <div className='price'>200000.00</div>
               </div>
